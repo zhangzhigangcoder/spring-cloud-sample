d7e2fba165135494ef7519a3069f9689ecdae78f
zhangzhigang <zhangzhigang_tech@163.com>
2020-03-29 23:08:23 +0800

    !

--- sample-discovery-eureka/src/main/java/com/zhangzhigang/cloud/EurekaApplication.java
+++ sample-discovery-eureka/src/main/java/com/zhangzhigang/cloud/EurekaApplication.java
@@ -3,6 +3,9 @@ package com.zhangzhigang.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,4 +13,14 @@ public class EurekaApplication {
   public static void main(String[] args) {
     SpringApplication.run(EurekaApplication.class, args);
   }
+  
+  @EnableWebSecurity
+	static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().ignoringAntMatchers("/eureka/**");
+			super.configure(http);
+		}
+	}
 }
