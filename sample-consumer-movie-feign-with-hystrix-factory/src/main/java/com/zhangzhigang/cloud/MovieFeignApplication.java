89bd277451edf6913468cdb9dc21c68295b323dd
zhangzhigang <zhangzhigang_tech@163.com>
2020-03-30 10:51:45 +0800

    ！

--- sample-consumer-movie-feign-with-hystrix-factory/src/main/java/com/zhangzhigang/cloud/MovieFeignApplication.java
+++ sample-consumer-movie-feign-with-hystrix-factory/src/main/java/com/zhangzhigang/cloud/MovieFeignApplication.java
@@ -3,7 +3,7 @@ package com.zhangzhigang.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
