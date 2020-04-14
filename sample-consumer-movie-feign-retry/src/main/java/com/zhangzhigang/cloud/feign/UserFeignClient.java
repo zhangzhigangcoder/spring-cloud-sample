d832b3233c41c2ba1b116f522c59147cdf0c6031
zhangzhigang <zhangzhigang_tech@163.com>
2020-04-14 13:41:17 +0800

    ！

---
+++ sample-consumer-movie-feign-retry/src/main/java/com/zhangzhigang/cloud/feign/UserFeignClient.java
@@ -0,0 +1,24 @@
+package com.zhangzhigang.cloud.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.zhangzhigang.cloud.entity.User;
+
+// 支持客户端负载均衡 Ribbon
+@FeignClient("microservice-provider-user")
+public interface UserFeignClient {
+	
+  @GetMapping("/simple/{id}")
+  public User findById(@PathVariable("id") Long id); // @PathVariable得设置value
+
+  @PostMapping("/user")
+  public User postUser(@RequestBody User user);
+
+  // 该请求不会成功，只要参数是复杂对象，即使指定了是GET方法，feign依然会以POST方法进行发送请求。可能是我没找到相应的注解或使用方法错误。
+  @GetMapping("/get-user")
+  public User getUser(User user);
+}
--- sample-discovery-eureka/src/main/java/com/zhangzhigang/cloud/EurekaApplication.java
+++ sample-discovery-eureka/src/main/java/com/zhangzhigang/cloud/EurekaApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+	
   public static void main(String[] args) {
     SpringApplication.run(EurekaApplication.class, args);
   }
