f7d4f8ff2e9dbbe1d0ff87f8c6986eba1f969452
zhangzhigang <zhangzhigang_tech@163.com>
2020-05-05 21:01:10 +0800

    ！

--- sample-gateway-zuul/src/main/java/com/zhangzhigang/cloud/ZuulApplication.java
+++ sample-gateway-zuul/src/main/java/com/zhangzhigang/cloud/ZuulApplication.java
@@ -7,6 +7,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 /**
  * 演示:
  * 	利用zuul实现微服务路由功能
+ *  RibbonRoutingFilter
+ *  SimpleHostRoutingFilter
+ *  SendForwardFilter
+ *  
+ *  HttpClientRibbonCommand
+ *  AbstractRibbonCommand.run() : 真正执行路由逻辑
  * 此处不需要用 @EnableEurekaClient注解就可以直接注册到eureka
  * @author zhangzhigang
  * @date 2018年5月15日 下午2:48:19
